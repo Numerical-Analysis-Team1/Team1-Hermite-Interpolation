5b4928deeee3efd3fe18176ab41134ad33645362
Adane <adaneadgo1@gmail.com>
2021-07-01 21:32:42 +0300

    function problem

---
+++ main.py
@@ -0,0 +1,32 @@
+import sympy as sp
+from sympy.utilities.lambdify import lambdify
+x = sp.sympify('x')
+
+
+def Lagrange(points_list):
+    mulls = []
+    mull = 1
+    for i in range(len(points_list)):
+        for j in range(len(points_list)):
+            if i != j:
+                mull *= (x - points_list[j][0]) / (points_list[i][0] - points_list[j][0])
+        mulls.append(mull)
+        mull = 1
+    ans = 0
+    for i in range(len(points_list)):
+        ans += mulls[i] * points_list[i][1]
+    return ans
+
+
+def hermite_interpolation(points_list):
+    l = Lagrange(points_list)
+
+
+f = Lagrange([(1,1), (2,2), (3,3)])
+f = lambdify(x, f)
+f.diff()
+
+
+
+
+
